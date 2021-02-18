a4a6828b538d72e8099395d423043109bb2c0c37
rodcalrub <rodcalrub@alum.us.es>
2021-02-18 18:09:49 +0100

    Co-authored-by: Fredo Fuentes <FredoFuentesUS@users.noreply.github.com>

--- parser.py
+++ parser.py
@@ -1,15 +1,11 @@
 
 import os, json
-
+import pandas as pd
 from hierarchy import DisplayablePath
 # from pathlib import Path
 from data import *
-
-# from bad_back_round import 
-#bad_back_round, bad_back_warp, bad_head, bad_inner.thigh, bad_shallow, bad_toe, good
-#nombre de files: 0918_squat_000016.json
-
-
+from collections import OrderedDict
+from insider import parse
 #Path raiz de los archivos
 path = ''
 
@@ -17,28 +13,6 @@ path = ''
 #iterar sobre las subcarpetas ignorando 1115...
 #Iterar sobre los archivos json parseandolos almacenando por cada index que contienen la estructura:
 #nombreJson, index, list of 3djoints, nombre de la carpeta(label)
-
-# for (root,dirs,files) in walk('./data', topdown=True): 
-#         print (root) 
-#         print (dirs) 
-#         print(files[1])
-#         print ('--------------------------------') 
-
-
-    #print(path.displayable())
-
-import json
-from glob import glob
-
-data = []
-father = './data' 
-
-#pattern = path.join('/path/to/json/files', '*.json')
-for folder in glob(os.path.join(father)):
-    print(folder)
-    # for file_name in glob(pattern):
-    #     with open(file_name) as f:
-    #         data.append(json.load(f))
 
 
 #Pasamos las carpetas con los jsos a un JSON final:
@@ -48,7 +22,7 @@ for folder in glob(os.path.join(father)):
         # "0918_squat_000039.json",
         # "0918_squat_000040.json",
         # "0918_squat_000041.json", ....
-        
+
 fileList = {}
 def importJSONS(path):
     folders = [pos_json for pos_json in os.listdir(path)]
@@ -56,9 +30,21 @@ def importJSONS(path):
         if(os.path.isdir(str(path+"/"+folder))):
             sub_folders = os.listdir(path+"/"+folder)
             for json in sub_folders:
-                fileList[path+"/"+folder]=[json for json in os.listdir(path+"/"+folder+'/'+json) if json.endswith('.json')]           
+                fileList[path+"/"+folder+'/'+json]=[json for json in os.listdir(path+"/"+folder+'/'+json) if json.endswith('.json')]           
     return fileList
 
 
 jsonF = importJSONS('D:/squat-clasifier/data')
-print(json.dumps(jsonF, indent=4))
+#print(json.dumps(jsonF, indent=4))
+
+
+for jk in jsonF.keys():
+    # parse(json.load(js))
+    for json_inside in jsonF[jk]:
+         with open(jk+'/'+json_inside,'r') as f:
+            print()
+            listaFinal = parse(json.load(f))
+
+print(listaFinal)
+# df1 = pd.DataFrame.from_dict(data=OrderedDict(d.items()),  orient='index',columns=[])
+# df1.to_csv(path+"\\csv_export\\csvFinal.csv")
